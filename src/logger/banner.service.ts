687347b00bc324824d626883d537653b757d6d14
Noah Pirthauer <noah.pirthauer@gmail.com>
2025-05-13 21:36:11 +0200

    Minor change.

--- src/logger/banner.service.ts
+++ src/logger/banner.service.ts
@@ -38,7 +38,7 @@ export class BannerService implements OnApplicationBootstrap {
      */
     onApplicationBootstrap() {
         const { host, nodeEnv, port } = nodeConfig;
-        figlet('HARDWARE M2025.1.0', (_, data) => console.info(data));
+        figlet('HARDWARE   M2025.1.0', (_, data) => console.info(data));
         // https://nodejs.org/api/process.html
         // "Template String" ab ES 2015
         this.#logger.info('Node: %s', process.version);
